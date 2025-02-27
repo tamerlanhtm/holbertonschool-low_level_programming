afadca4b7ab5d7eff422818ad2cb9ea961470b0d
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-02-27 11:16:52 +0000

    3 4 5

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *  main - This is the entry point of the code
+ *
+ *  Return: 0 Successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
