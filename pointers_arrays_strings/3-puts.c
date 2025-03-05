63ff17040db2b1bd53eaf36c0f2bfa20266db1df
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-05 15:39:57 +0000

    3467

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,14 @@
-a
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
