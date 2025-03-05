18006ab3de9797a374d3eccb3c605601c35ae740
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-05 20:16:41 +0400

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,15 +8,9 @@ void print_rev(char *s)
 {
     int len = 0;
     int i;
-    
-    /* Calculate string length */
     while (s[len] != '\0')
         len++;
-    
-    /* Print characters in reverse */
     for (i = len - 1; i >= 0; i--)
         _putchar(s[i]);
-    
-    /* Print newline */
     _putchar('\n');
 }
