365e11d08fc091e48cb1f20fea960aa40e854579
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-06 13:39:38 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,31 +10,21 @@
  */
 char *_strcat(char *dest, char *src) {
     char *ptr = dest;
-    
-    // Move the pointer to the end of dest
+
+    /* Move the pointer to the end of dest */
     while (*ptr != '\0') {
         ptr++;
     }
-    
-    // Append src to dest
+
+    /* Append src to dest */
     while (*src != '\0') {
         *ptr = *src;
         ptr++;
         src++;
     }
-    
-    // Add null terminator
-    *ptr = '\0';
-    
-    return dest;
-}
 
-int main() {
-    char str1[100] = "Hello, ";  // Ensure str1 has enough space
-    char str2[] = "World!";
-    
-    _strcat(str1, str2);
-    printf("%s\n", str1);
-    
-    return 0;
+    /* Add null terminator */
+    *ptr = '\0';
+
+    return dest;
 }
