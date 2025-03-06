8147c46ab633811557d53f45e083dc4b29c21b86
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-06 13:43:41 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,23 +8,24 @@
  *
  * Return: A pointer to the resulting string @dest.
  */
-char *_strcat(char *dest, char *src) {
-    char *ptr = dest;
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
 
-    /* Move the pointer to the end of dest */
-    while (*ptr != '\0') {
-        ptr++;
-    }
+	/* Move the pointer to the end of dest */
+	while (*ptr != '\0')
+		ptr++;
 
-    /* Append src to dest */
-    while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
+	/* Append src to dest */
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
 
-    /* Add null terminator */
-    *ptr = '\0';
+	/* Add null terminator */
+	*ptr = '\0';
 
-    return dest;
+	return (dest);
 }
