57ac06070ca16536cbc15a47128be6d1a6b297b6
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-10 07:47:01 +0000

    1

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
  *
  * Return: A pointer to the filled memory area @s.
  */
-char *_memset(char *s, char b, unsigned int n)
+char *memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
     for (i = 0; i < n; i++)
@@ -19,3 +19,4 @@ char *_memset(char *s, char b, unsigned int n)
     }
     return s;
 }
+
