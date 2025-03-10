274853a93fc1e13b7e95951f0a54bcad7b68893a
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-10 10:31:44 +0000

    16

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line
- * @s: string to print
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: pointer to a char.
  */
 void _puts_recursion(char *s)
 {
@@ -11,6 +10,7 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
+
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
